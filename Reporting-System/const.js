d52645f6462ef936f1d3ed965ee55d5ea87cf704
Lithium <ahmedellithyy@gmail.com>
2022-03-19 12:31:19 +0200

    Create const.js

---
+++ Reporting-System/const.js
@@ -0,0 +1,53 @@
+// The name of the LC to be used in the report
+const lcName = "Menofia"
+// the indices of the rows and columns related to the analysis of the ICX & OGX
+const indices ={
+  "OGX_Severity_Row":27,
+  "OGX_Fine_Row":28,
+  "ICX_Severity_Row":55,
+  "ICX_Fine_Row":56,
+  "GV_Pass_Col":33,
+  "GTe_Pass_Col":42,
+  "GTa_Pass_Col":42,
+}
+// The columns of the months of auditing to be used in auditing report
+const commonMistakesCol ={
+  "February2022":2,
+  "March2022":4,
+  "April2022":6,
+  "May2022":8,
+  "June2022":10,
+  "July2022":12,
+  "August2022":14,
+  "September2022":16,
+  "October2022":18,
+  "November2022":20,
+  "December2022":22,
+}
+// the rows of of common mistakes of auditing to be used in the auditing report
+const commonMistakesRow={
+  "OGXAPDs":3,
+  "OGXAPDsToBeBroken":4,
+  "OGXREs":5,
+  "OGVFIs":6,
+  "OGTMonthly":7,
+  "OGTFIs":8,
+  "OGXStandards":9,
+  "OPS":10,
+
+  "ICXAPDs":12,
+  "ICXAPDsToBeBroken":13,
+  "ICXILs":14,
+  "ICXREs":15,
+  "IGVFIs":16,
+  "IGTMonthly":17,
+  "IGTFIs":18,
+  "ICXStandards":19,
+  "IPS":20,
+  "Accommodation":21
+}
+// The month corresponded to its index
+const monthNames = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+
+
+const driveFolderId = "15VC14Jqoc066zKkPjxejvK_I-l0z6y6j"
