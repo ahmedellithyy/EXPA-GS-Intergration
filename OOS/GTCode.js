901dbca43ec01612f8ff05bd62086beec548b5de
Lithium <ahmedellithyy@gmail.com>
2022-07-25 16:16:57 +0200

    consts replacing

--- OOS/GTCode.js
+++ OOS/GTCode.js
@@ -1,72 +1,86 @@
-function updateOpportunityData_GT(){
-  var sheet = SpreadsheetApp.getActiveSpreadsheet().getSheetByName("GTa/e Opportunities")
-  var oppData = sheet.getRange(4,1,sheet.getLastRow()-3,sheet.getLastColumn()).getValues()
+/*------------------------------------------------------------------------ Main Function ------------------------------------------------------------------------*/
+function updateOpportunityData(){
+
+  // Get the sheet of the IGT opportunities
+  var sheet = SpreadsheetApp.getActiveSpreadsheet().getSheetByName(`${gtSheet}`)
+  var oppData = sheet.getRange(gtHeaderRow+1,1,sheet.getLastRow()-gtHeaderRow,sheet.getLastColumn()).getValues()
   var lr = getLastRowForSheet(oppData)
+
   for(let i =0; i < lr; i++){
     try{
-      var response = UrlFetchApp.fetch(`https://gis-api.aiesec.org/v2/opportunities/${oppData[i][7]}?access_token=2f4c006eecfe5fb616839f27a4731c19cd7faecc11e1c97afd0ccc86491e207d`);  
+      var response = UrlFetchApp.fetch(`https://gis-api.aiesec.org/v2/opportunities/${oppData[i][opportunity_id_column_GT-1]}?access_token=${access_token}`);  
       var recievedDate = JSON.parse(response.getContentText());
-      sheet.getRange(i+4,1).setValue(recievedDate.status)
-      if(recievedDate.home_lc.parent_id == 1609){
+      sheet.getRange(i+gvHeaderRow+1,expa_status_column_GT).setValue(recievedDate.status)
+
+      if(recievedDate.home_lc.parent_id == egypt_expa_id){
     Logger.log(i)
         if(recievedDate.opened_by != null){
-          sheet.getRange(i+4,2).setValue(recievedDate.opened_by.full_name)
+          sheet.getRange(i+gvHeaderRow+1,opened_by_name_column_GT).setValue(recievedDate.opened_by.full_name)
         }
         else{
-          sheet.getRange(i+4,2).setValue("-")
+          sheet.getRange(i+gvHeaderRow+1,opened_by_name_column_GT).setValue("-")
         }
         if(recievedDate.status == "open"){
-            sheet.getRange(i+4,3).setValue(recievedDate.updated_at.toString().substring(0,10))     
-          if((oppData[i][41]=="Closed" && oppData[i][4] == "Audited & Not Passed") || oppData[i][3] != "Passed"){
-            var options = {
-              "method":"DELETE"
-            }
-            UrlFetchApp.fetch(`https://gis-api.aiesec.org/v2/opportunities/${oppData[i][7]}?access_token=2f4c006eecfe5fb616839f27a4731c19cd7faecc11e1c97afd0ccc86491e207d`,options)
-            sheet.getRange(i+4,1).setValue("removed as it didn't pass")
-            let message = `Dear ${oppData[i][10]},\nYour Opportunity that has this ID ${oppData[i][7]} got closed becuase it's not approved by ECB or MCVP as it doesn't meet opportunity guidelines.`
-            GmailApp.sendEmail(oppData[i][9],`Your Opportunity Got Closed - Opportunity ID: ${oppData[i][7]}`,message,
-            { body:message,
-              cc: "ahmed.ellithy4@aiesec.net,adel.mohamed@aiesec.net,lujain.shaltout@aiesec.net,c.safwat@aiesec.org.eg"}
 
+            sheet.getRange(i+gvHeaderRow+1,updated_at_column_GT).setValue(recievedDate.updated_at.toString().substring(0,10))     
+
+          if((oppData[i][closed_by_ecb_column_GT-1]=="Closed" && oppData[i][audited_by_ecb_column_GT-1] == "Audited & Not Passed") || oppData[i][audited_by_mc_column_GT-1] != "Passed"){
+            
+            UrlFetchApp.fetch(`https://gis-api.aiesec.org/v2/opportunities/${oppData[i][opportunity_id_column_GT-1]}?access_token=${access_token}`,options_delete)
+            sheet.getRange(i+gvHeaderRow+1,expa_status_column_GT).setValue("removed as it didn't pass")
+            let message = `Dear ${oppData[i][full_name_column_GT-1]},\nYour Opportunity that has this ID ${oppData[i][opportunity_id_column_GT-1]} got closed becuase it's not approved by ECB or MCVP as it doesn't meet opportunity guidelines.`
+
+            GmailApp.sendEmail(oppData[i][email_column_GT-1],`Your Opportunity Got Closed - Opportunity ID: ${oppData[i][opportunity_id_column_GT-1]}`,message,
+            { body:message,
+              cc: emails_for_the_GT_submissions
+              }
             )
-            sheet.getRange(i+4,3).setValue(recievedDate.updated_at.toString().substring(0,10))     
+            sheet.getRange(i+gvHeaderRow+1,updated_at_column_GT).setValue(recievedDate.updated_at.toString().substring(0,10))     
           }
         }
         else if(recievedDate.status == "removed"){
-          sheet.getRange(i+4,3).setValue(recievedDate.updated_at.toString().substring(0,10))
+          sheet.getRange(i+gvHeaderRow+1,updated_at_column_GT).setValue(recievedDate.updated_at.toString().substring(0,10))
         }
         
       }
       else{
-        sheet.getRange(i+4,1).setValue("Wrong Opportunity ID")
-        if(oppData[i][43] != true){
-          let message = `Dear ${oppData[i][10]},\nYou have entered a wrong opportunity ID in Opportunities Openning System Form which its role title is ${oppData[i][12]}. PLEASE UPDATE it with the right one (don't do re-submission to avoid having duplicate submissions on the system which will cause your opportunity to get automatically closed even if one of the submissions is correct)!`
-          GmailApp.sendEmail(oppData[i][9],`Wrong Submission In Opportunities Openning System Form - ID ${oppData[i][7]}`,message,
+        sheet.getRange(i+gvHeaderRow+1,expa_status_column_GT).setValue("Wrong Opportunity ID")
+        if(oppData[i][wrong_opp_id_colum_GT-1] != true){
+          let message = `Dear ${oppData[i][full_name_column_GT-1]},\nYou have entered a wrong opportunity ID in Opportunities Openning System Form which its role title is ${oppData[i][opportunity_role_title_column_GT-1]}. PLEASE UPDATE it with the right one (don't do re-submission to avoid having duplicate submissions on the system which will cause your opportunity to get automatically closed even if one of the submissions is correct)!`
+          GmailApp.sendEmail(oppData[i][email_column_GT-1],`Wrong Submission In Opportunities Openning System Form - ID ${oppData[i][opportunity_id_column_GT-1]}`,message,
             {
               htmlBody:message,
-              cc: "ahmed.ellithy4@aiesec.net,adel.mohamed@aiesec.net,lujain.shaltout@aiesec.net,c.safwat@aiesec.org.eg"
+              cc: emails_for_the_GT_submissions
             }
           )
-          sheet.getRange(i+4,44).setValue(true)
+          sheet.getRange(i+gvHeaderRow+1,wrong_opp_id_colum_GT).setValue(true)
         }
       }
     }
     catch(err){ 
       try{
-          if(oppData[i][43] != true && err["message"].toString().includes("Couldn't find Opportunity") == true){
-            let message = `Dear ${oppData[i][10]},\nYou have entered a wrong opportunity ID in Opportunities Openning System Form which its role title is ${oppData[i][12]}. PLEASE UPDATE it with the right one (don't do re-submission to avoid having duplicate submissions on the system which will cause your opportunity to get automatically closed even if one of the submissions is correct)!`
-              GmailApp.sendEmail(oppData[i][9],`Wrong Submission In Opportunities Openning System Form - ID ${oppData[i][7]}`,message,
+          if(oppData[i][wrong_opp_id_colum_GT-1] != true && err["message"].toString().includes("Couldn't find Opportunity") == true){
+            let message = `Dear ${oppData[i][full_name_column_GT-1]},\nYou have entered a wrong opportunity ID in Opportunities Openning System Form which its role title is ${oppData[i][opportunity_role_title_column_GT-1]}. PLEASE UPDATE it with the right one (don't do re-submission to avoid having duplicate submissions on the system which will cause your opportunity to get automatically closed even if one of the submissions is correct)!`
+              GmailApp.sendEmail(oppData[i][email_column_GT-1],`Wrong Submission In Opportunities Openning System Form - ID ${oppData[i][opportunity_id_column_GT-1]}`,message,
               {
                 htmlBody:message,
-                cc: "ahmed.ellithy4@aiesec.net,adel.mohamed@aiesec.net,lujain.shaltout@aiesec.net,c.safwat@aiesec.org.eg"
+                cc: emails_for_the_GT_submissions
                 }
               )
-              sheet.getRange(i+4,44).setValue(true)
-              sheet.getRange(i+4,1).setValue("Wrong Opportunity ID")
+              sheet.getRange(i+gvHeaderRow+1,wrong_opp_id_colum_GT).setValue(true)
+              sheet.getRange(i+gvHeaderRow+1,expa_status_column_GT).setValue("Wrong Opportunity ID")
+          }
+          else{
+            let message = `Check OOS,\n ${e.toString()}`
+              GmailApp.sendEmail(mcvpIM,"ERROR IN OOS",message,
+              {
+                body:message,
+                }
+              )
           }
           }
           catch(err){
-              sheet.getRange(i+4,1).setValue("Wrong Opportunity ID & Wrong Email or No Email")
+              sheet.getRange(i+gvHeaderRow+1,expa_status_column_GT).setValue("Wrong Opportunity ID & Wrong Email or No Email")
           }
             
     }
@@ -74,64 +88,34 @@ function updateOpportunityData_GT(){
   }
 }
 
-function checkOpportunities_GT(){
-  var sheet = SpreadsheetApp.getActiveSpreadsheet().getSheetByName("Opportunities opened without submiting - GTa/e")
-  var sheetGT = SpreadsheetApp.getActiveSpreadsheet().getSheetByName("GTa/e Opportunities")
+function checkOpportunities(){
+  var sheet = SpreadsheetApp.getActiveSpreadsheet().getSheetByName(`${gtSheet_not_submitted}`)
+  var sheetGT = SpreadsheetApp.getActiveSpreadsheet().getSheetByName(`${gtSheet}`)
 
-  var response = UrlFetchApp.fetch("https://gis-api.aiesec.org/v2/opportunities?access_token=2f4c006eecfe5fb616839f27a4731c19cd7faecc11e1c97afd0ccc86491e207d&only=data&per_page=1000&filters[status]=open&filters[created]%5Bfrom%5D=01%2F12%2F2021")
+  var response = UrlFetchApp.fetch(`https://gis-api.aiesec.org/v2/opportunities?access_token=${access_token}&only=data&per_page=1000&filters[status]=open&filters[created]%5Bfrom%5D=01%2F12%2F2021`)
   var data = JSON.parse(response.getContentText()).data;
   var newRows = []
-  var options = {
-    "method":"DELETE"
-  }
+  
   for(let i = 0; i< data.length; i++){
     if(data[i].programmes.short_name == "GTa" || data[i].programmes.short_name == "GTe"){
       var row = sheetGT.createTextFinder(data[i].id).matchEntireCell(true).findAll().map(x => x.getRow())
       if(row.length ==0){
-        var responseOpp = UrlFetchApp.fetch(`https://gis-api.aiesec.org/v2/opportunities/${data[i].id}?access_token=2f4c006eecfe5fb616839f27a4731c19cd7faecc11e1c97afd0ccc86491e207d`);  
+        var responseOpp = UrlFetchApp.fetch(`https://gis-api.aiesec.org/v2/opportunities/${data[i].id}?access_token=${access_token}`);  
         var recievedOppDate = JSON.parse(responseOpp.getContentText());
         var today = new Date()
         newRows.push([
           data[i].id, data[i].title,  data[i].programmes.short_name,  data[i].office.full_name, "removed",today ,recievedOppDate.opened_by.full_name, 
           recievedOppDate.managers[0] != null? recievedOppDate.managers[0].full_name:""
         ])
-        UrlFetchApp.fetch(`https://gis-api.aiesec.org/v2/opportunities/${data[i].id}?access_token=2f4c006eecfe5fb616839f27a4731c19cd7faecc11e1c97afd0ccc86491e207d`,options)
-        GmailApp.sendEmail("c.safwat@aiesec.org.eg,lujain.shaltout@aiesec.net","GTa/e Opportunity Got Closed",`The opportunity with this id (${data[i].id}) got closed because it's not submitted in The opportunity Opening System. So please, contact the LC to submit it.`)
+        UrlFetchApp.fetch(`https://gis-api.aiesec.org/v2/opportunities/${data[i].id}?access_token=${access_token}`,options_delete)
+        GmailApp.sendEmail(`${emails_for_the_GT_submissions}`,"GTa/e Opportunity Got Closed",`The opportunity with this id (${data[i].id}) got closed because it's not submitted in The opportunity Opening System. So please, contact the LC to submit it.`)
       }
     }
   }
   if(newRows.length > 0){
-    sheet.getRange(2,1,sheet.getLastRow(),sheet.getLastColumn()).clearContent()
-    sheet.getRange(2,1,newRows.length,newRows[0].length).setValues(newRows)
+    sheet.getRange(gtvSheet_not_submitted_header_row+1,1,sheet.getLastRow(),sheet.getLastColumn()).clearContent()
+    sheet.getRange(gtvSheet_not_submitted_header_row+1,1,newRows.length,newRows[0].length).setValues(newRows)
   }
-}
-
-function sendEmailonSubmissions(){
-  GmailApp.sendEmail("c.safwat@aiesec.org.eg,lujain.shaltout@aiesec.net","New Submission in Opportunity Opening System","Check the OOS for checking the new submission.") 
-}
-
-function fillFormulas_GT(){
-  var sheet = SpreadsheetApp.getActiveSpreadsheet().getSheetByName("GTa/e Opportunities")
-  var oppData = sheet.getRange(4,1,sheet.getLastRow()-3,sheet.getLastColumn()).getValues()
-  var lr = getLastRowForSheet(oppData)
-  
-  for(let i = 0; i < lr; i++){
-    sheet.getRange(i+4,4).setFormula(`=if(I${i+4}<>"",ifs(AND(AA${i+4}="Correct",AB${i+4}="Correct",AC${i+4}="Correct",AD${i+4}="Correct",AE${i+4}="Correct",AF${i+4}="Correct",AG${i+4}="Correct"),"Passed",AND(AA${i+4}="",AB${i+4}="",AC${i+4}="",AD${i+4}="",AE${i+4}="",AF${i+4}="",AG${i+4}=""),"Not Checked",OR(AA${i+4}="",AB${i+4}="",AC${i+4}="",AD${i+4}="",AE${i+4}="",AF${i+4}="",AG${i+4}=""),"Not Fully Checked",OR(AA${i+4}="Incorrect",AB${i+4}="Incorrect",AC${i+4}="Incorrect",AD${i+4}="Incorrect",AE${i+4}="Incorrect",AF${i+4}="Incorrect",AG${i+4}="Incorrect"),"Not Passed"),"")`)
-    
-    sheet.getRange(i+4,5).setFormula(`=if(I${i+4}<>"",ifs(AND(AK${i+4}="Correct",AL${i+4}="Correct",AM${i+4}="Correct",AN${i+4}="Correct",AO${i+4}="Correct"),"Audited & Passed",AND(AK${i+4}="",AL${i+4}="",AM${i+4}="",AN${i+4}="",AO${i+4}=""),"Not Audited",OR(AK${i+4}="",AL${i+4}="",AM${i+4}="",AN${i+4}="",AO${i+4}=""),"Not Fully Audited",OR(AK${i+4}="Incorrect",AL${i+4}="Incorrect",AM${i+4}="Incorrect",AN${i+4}="",AO${i+4}="Incorrect"),"Audited & Not Passed"),"")`)
-    
-    sheet.getRange(i+4,6).setFormula(`=if(I${i+4}<>"",year(I${i+4})&"-"&if(AND(month(I${i+4})<>10,month(I${i+4})<>11,month(I${i+4})<>12),"0"&month(I${i+4}),month(I${i+4})),"")`)
-  }
-}
-
-function getLastRowForSheet(sheetData){
-  var lr = 0
-  for(let i = 0; i < sheetData.length;i++ ){
-    if(sheetData[i][6] != ""){
-      lr++
-    }
-  }
-  return lr
 }
 
 
